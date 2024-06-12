58865db7df2e09ccc098abee996f29be506eb106
Bruno Cabral <cabral.brc@gmail.com>
2024-06-12 12:09:19 -0700

    sort draw order
    Signed-off-by: Bruno Cabral <cabral.brc@gmail.com>

--- code/groups.cpp
+++ code/groups.cpp
@@ -2,30 +2,45 @@
 #include <raymath.h>
 #include "settings.h"
 
+#include <algorithm>
+
 void AllSprites::Draw(const Vector2 player_center)
 {
     offset = Vector2Subtract(player_center, Vector2Scale({WINDOW_WIDTH, WINDOW_HEIGHT}, 0.5));
     offset = Vector2Scale(offset, -1);
 
+    std::vector<SimpleSprite *> bg_sprites;
+    std::vector<SimpleSprite *> main_sprites;
+    std::vector<SimpleSprite *> top_sprites;
+
     for (const auto &sprite: sprites)
     {
         if (sprite->z < WORLD_LAYERS["main"])
         {
-            sprite->Draw(offset);
+            bg_sprites.push_back(sprite);
+        }
+        else if (sprite->z == WORLD_LAYERS["main"])
+        {
+            main_sprites.push_back(sprite);
+        }
+        else if (sprite->z > WORLD_LAYERS["main"])
+        {
+            top_sprites.push_back(sprite);
         }
     }
-    for (const auto &sprite: sprites)
+    std::sort(
+            main_sprites.begin(), main_sprites.end(),
+            [](const SimpleSprite *l, const SimpleSprite *r) { return l->z < r->z; });
+    for (const auto bg: bg_sprites)
     {
-        if (sprite->z == WORLD_LAYERS["main"])
-        {
-            sprite->Draw(offset);
-        }
+        bg->Draw(offset);
     }
-    for (const auto &sprite: sprites)
+    for (const auto main: main_sprites)
     {
-        if (sprite->z > WORLD_LAYERS["main"])
-        {
-            sprite->Draw(offset);
-        }
+        main->Draw(offset);
+    }
+    for (const auto top: top_sprites)
+    {
+        top->Draw(offset);
     }
 }
