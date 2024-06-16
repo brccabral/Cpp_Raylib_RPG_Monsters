7ebd8905ca772b04027715b2c4a33a5b89d50046
Bruno Cabral <cabral.brc@gmail.com>
2024-06-16 02:49:05 -0700

    MonsterIndex::DisplayList init
    Signed-off-by: Bruno Cabral <cabral.brc@gmail.com>

--- code/monsterindex.cpp
+++ code/monsterindex.cpp
@@ -1,26 +1,44 @@
 #include "monsterindex.h"
+
+#include "raylib_utils.h"
 #include "settings.h"
 
 
-MonsterIndex::MonsterIndex(std::vector<Monster> monsters, const std::map<std::string, Font> &fonts) : fonts(fonts)
+MonsterIndex::MonsterIndex(const std::vector<Monster> &monsters, const std::map<std::string, Font> &fonts)
+    : fonts(fonts), monsters(monsters)
 {
+    RectToCenter(main_rect, {WINDOW_WIDTH / 2.0f, WINDOW_HEIGHT / 2.0f});
+
     tint_surface = LoadRenderTexture(WINDOW_WIDTH, WINDOW_HEIGHT);
     BeginTextureMode(tint_surface);
     ClearBackground(BLACK);
     DrawRectangle(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, Fade(BLACK, 200.0f / 255.0f));
+    DrawRectangleRec(main_rect, BLACK);
     EndTextureMode();
 }
 
-void MonsterIndex::Update(double dt)
+void MonsterIndex::Update(double dt) const
 {
     // input
 
-    // tint main game
     BeginTextureMode(display_surface);
+    // tint main game
     DrawTexture(tint_surface.texture, 0, 0, WHITE);
-    EndTextureMode();
 
     // display list
+    DisplayList();
+    EndTextureMode();
 
     // display the main section
+}
+
+void MonsterIndex::DisplayList() const
+{
+    for (int i = 0; i < monsters.size(); ++i)
+    {
+        const float top = main_rect.y + i * item_height;
+        const Rectangle item_rect = {main_rect.x, top, list_width, item_height};
+        // BeginTextureMode was called in Update()
+        DrawRectangleRec(item_rect, Fade(COLORS["red"], (i + 1.0f) / monsters.size()));
+    }
 }
--- code/monsterindex.h
+++ code/monsterindex.h
@@ -2,17 +2,25 @@
 #include <vector>
 #include <raylib.h>
 #include "monster.h"
+#include "settings.h"
 
 
 class MonsterIndex
 {
 public:
 
-    MonsterIndex(std::vector<Monster> monsters, const std::map<std::string, Font> &fonts);
-    void Update(double dt);
+    MonsterIndex(const std::vector<Monster> &monsters, const std::map<std::string, Font> &fonts);
+    void Update(double dt) const;
 
 private:
 
+    void DisplayList() const;
+
     RenderTexture2D tint_surface{};
     std::map<std::string, Font> fonts;
+    Rectangle main_rect{0, 0, WINDOW_WIDTH * 0.6, WINDOW_HEIGHT * 0.8};
+    std::vector<Monster> monsters;
+    int visible_items = 6;
+    float list_width = main_rect.width * 0.3;
+    float item_height = main_rect.height / visible_items;
 };
