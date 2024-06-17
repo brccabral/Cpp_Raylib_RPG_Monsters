7079a277c38968582dccd8518be3db7d26a26331
Bruno Cabral <cabral.brc@gmail.com>
2024-06-16 23:45:30 -0700

    organize abilities
    Signed-off-by: Bruno Cabral <cabral.brc@gmail.com>

--- code/monsterindex.cpp
+++ code/monsterindex.cpp
@@ -205,11 +205,13 @@ void MonsterIndex::DisplayMain(const double dt)
     auto abilities = monster.GetAbilities();
     for (int a_index = 0; a_index < abilities.size(); ++a_index)
     {
-        float x = ability_rect.x;
-        float y = 20.f + ability_rect.y + a_index * fonts["regular"].baseSize;
+        float x = ability_rect.x + a_index % 2 * ability_rect.width / 2;
+        float y = 20.f + ability_rect.y + int(a_index / 2) * (fonts["regular"].baseSize + 20);
         Vector2 ability_pos = {x, y};
         Vector2 ability_text_size = MeasureTextF(fonts["regular"], abilities[a_index].c_str(), 1);
-        DrawRectangleV(ability_pos, ability_text_size, COLORS["white"]);
+        Rectangle ability_rect = {ability_pos.x, ability_pos.y, ability_text_size.x, ability_text_size.y};
+        RectInflate(ability_rect, 10, 10);
+        DrawRectangleRounded(ability_rect, 0.3, 10, COLORS["white"]);
         DrawTextEx(
                 fonts["regular"], abilities[a_index].c_str(), ability_pos, fonts["regular"].baseSize, 1,
                 COLORS["black"]);
