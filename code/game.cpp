1ee009925231c9fa5884b0095266f0052e65e796
Bruno Cabral <cabral.brc@gmail.com>
2024-07-31 23:22:15 -0700

    show tmx layer Terrain

--- code/game.cpp
+++ code/game.cpp
@@ -7,6 +7,9 @@ Game::Game()
     rg::Init();
     display_surface = rg::display::SetMode(WINDOW_WIDTH, WINDOW_HEIGHT);
     rg::display::SetCaption("RPG Monsters");
+
+    ImportAssets();
+    Setup(tmx_maps["world"], "house");
 }
 
 Game::~Game()
@@ -18,7 +21,26 @@ void Game::run()
 {
     while (!rl::WindowShouldClose())
     {
-        display_surface->Fill(rl::RED);
         rg::display::Update();
+    }
+}
+
+void Game::ImportAssets()
+{
+    tmx_maps["world"] = rl::LoadTMX("resources/data/maps/world.tmx");
+}
+
+void Game::Setup(const rl::tmx_map *tmx_map, std::string player_start_pos)
+{
+    const auto *terrain_layer = tmx_find_layer_by_name(tmx_map, "Terrain");
+    const auto terrain_surface = rg::tmx::GetTMXLayerSurface(tmx_map, terrain_layer);
+    display_surface->Blit(terrain_surface);
+}
+
+void Game::UnloadResources()
+{
+    for (auto &[key, tmx_map]: tmx_maps)
+    {
+        UnloadTMX(tmx_map);
     }
 }
--- code/game.h
+++ code/game.h
@@ -12,5 +12,10 @@ public:
 
 private:
 
+    void ImportAssets();
+    void Setup(const rl::tmx_map *tmx_map, std::string player_start_pos);
+    void UnloadResources();
+
     rg::Surface *display_surface = nullptr;
+    std::map<std::string, rl::tmx_map *> tmx_maps;
 };
