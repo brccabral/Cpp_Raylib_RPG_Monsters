24bfe51379d9d40127d6837f0145dd54e168195d
Bruno Cabral <cabral.brc@gmail.com>
2024-08-02 20:50:30 -0700

    set display for manual render

--- code/groups.cpp
+++ code/groups.cpp
@@ -11,8 +11,10 @@ void AllSprites::Draw(const Player *player)
     offset = player->rect.center() - rg::math::Vector2{WINDOW_WIDTH / 2.0f, WINDOW_HEIGHT / 2.0f};
     offset *= -1.0f;
 
+    display_surface->StartRender(true);
     for (const auto *sprite: Sprites())
     {
-        display_surface->Blit(sprite->image, sprite->rect.topleft() + offset);
+        display_surface->Blit(sprite->image, sprite->rect.topleft() + offset, rl::BLEND_ALPHA);
     }
+    display_surface->EndRender(true);
 }
